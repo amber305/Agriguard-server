a55cd7b4e5fe02c29d9aa74ad8ddf8b220d22583
Amber Kaushik <138994052+amber305@users.noreply.github.com>
2025-03-18 10:54:50 +0530

    Update server.py

--- server.py
+++ server.py
@@ -111,4 +111,5 @@ def index():
     return jsonify({'message': 'Model is ready to predict'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+     port = int(os.getenv('PORT', 5000))
+     app.run(host='0.0.0.0', port=port)
